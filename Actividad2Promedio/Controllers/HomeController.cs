0619167c8316991c705828cac579478707d47562
191G0250@rcarbonifera.tecnm.mx <191G0250@rcarbonifera.tecnm.mx>
2022-09-22 18:05:18 -0500

    Agregar archivos de proyecto.

---
+++ Actividad2Promedio/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Actividad2Promedio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Promedio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void ObtenerPromedio(Promedio calificaciones)
+        {
+            calificaciones.Promedioo = (calificaciones.Calificacion1 + calificaciones.Calificacion2 + calificaciones.Calificacion3) / 3;
+            if (calificaciones.Promedioo >= 70)
+            {
+                calificaciones.Estado = "Aprobado";
+            }
+            else calificaciones.Estado = "Reprobado";
+        }
+        public IActionResult Index(Promedio pro)
+        {
+
+            ObtenerPromedio(pro);
+            return View(pro);
+        }
+    }
+}
---
+++ Actividad2Promedio/Models/Promedio.cs
@@ -0,0 +1,45 @@
+﻿namespace Actividad2Promedio.Models
+{
+    public class Promedio
+    {
+		private double calificacion1;
+
+		public double Calificacion1
+		{
+			get { return calificacion1; }
+			set { calificacion1 = value; }
+		}
+        private double calificacion2;
+
+        public double Calificacion2
+        {
+            get { return calificacion2; }
+            set { calificacion2 = value; }
+        }
+        private double calificacion3;
+
+        public double Calificacion3
+        {
+            get { return calificacion3 ; }
+            set { calificacion3 = value; }
+        }
+        private string estado;
+
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+        private double promedio;
+
+        public double Promedioo
+        {
+            get { return promedio; }
+            set { promedio = value; }
+        }
+
+
+
+    }
+}
